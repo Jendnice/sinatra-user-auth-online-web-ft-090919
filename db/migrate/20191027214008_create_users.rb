3a9064c6d6820aaeb68ab001c8ac328258a08bba
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-27 21:42:31 +0000

    Automatically backed up by Learn

--- db/migrate/20191027214008_create_users.rb
+++ db/migrate/20191027214008_create_users.rb
@@ -3,8 +3,8 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name 
-      t.email :email 
-      t.password :password 
+      t.string :email 
+      t.string :password 
     end 
   end
   
